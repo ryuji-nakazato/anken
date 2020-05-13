79e1d458625ac6a42c04332b4d100e84b30acb77
zhonglilonger <ryuji.nakazato@g.softbank.co.jp>
2020-05-13 19:10:19 +0900

    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' を追加

--- anken/settings.py
+++ anken/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'anken.urls'
 
 TEMPLATES = [
